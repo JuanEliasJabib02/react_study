4d712b1eca109513f7e6e3d5b74b7b1dd1233170
JuanEliasJabib02 <juaneliasjabib02@gmail.com>
2023-01-11 17:31:53 -0500

    studyng

---
+++ class 12/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import {Route, Routes ,Link} from "react-router-dom"
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Home from './components/Home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <header>Im the header
+        <nav>
+          <ul>
+          <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          </ul>
+        </nav>
+      </header>
+      < Routes >
+        < Route path="/" element={<Home />} />
+        < Route path="/about" element={<h1>About</h1>} />
+        < Route path="/contact" element={<h1>Contact</h1>} />
+      </Routes>
+
+      <footer> Im the footer</footer>
+    </div>
+  )
+}
+
+export default App
